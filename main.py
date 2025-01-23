865a028ca0fd06fbe2c2183acf178eb3e6cc1e2a
Ayush Sharma <kshitij3160@gmail.com>
2025-01-23 10:23:44 +0530

    feat: added frontend
    Signed-off-by: Ayush Sharma <kshitij3160@gmail.com>

--- main.py
+++ main.py
@@ -1,13 +1,21 @@
+import os
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
 from pydantic import BaseModel
 import pandas as pd
 from src.pipeline.predict_pipeline import predict as predict_price
 
 app = FastAPI()
 
-@app.get("/")
-def helloWorld():
-    return "Hello World" 
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+@app.get("/",response_class=HTMLResponse)
+def serve_homepage():
+    file_path = os.path.join(os.getcwd(), "static", "index.html")
+    with open(file_path, "r") as file:
+        html_content = file.read()
+    return HTMLResponse(content=html_content) 
 class Features(BaseModel):
     income: float
     house_age: float
